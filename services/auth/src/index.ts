60164cfddd287412ece8fbe3cb485260a8db3103
Vijay Singh <vijayrana98120@gmail.com>
2026-05-19 10:19:58 +0530

    Initial commit

---
+++ services/auth/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import authRoute from './routes/auth.js';
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+app.use("/api/auth", authRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Auth service is running on port ${PORT}`);
+    connectDB()
+})
+
---
+++ services/auth/src/routes/auth.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { addUserRole, loginUser, myProfile } from '../controllers/auth.js';
+import { isAuth } from '../middleware/isAuth.js';
+
+const router = express.Router();
+
+router.post("/login", loginUser); 
+router.put("/add/role",isAuth,addUserRole)
+router.get("/me",isAuth,myProfile)
+export default router;
