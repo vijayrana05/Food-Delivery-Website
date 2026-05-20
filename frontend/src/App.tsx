1c7ad7b579bd7b7df8e4a0e7e2bf6c2d233ac0db
Vijay Singh <vijayrana98120@gmail.com>
2026-05-20 14:26:59 +0530

    added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,39 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Home from "./pages/Home"
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast"
 import { useAppStore } from "./store/AppStore"
 import { useEffect } from "react"
 import PublicRoute from "./components/publicRoute"
 import ProtectedRoute from "./components/protectedRoute"
 import SelectRole from "./pages/SelectRole"
+import Navbar from "./components/navbar"
 const App = () => {
   const fetchUser = useAppStore((state) => state.fetchUser)
 
   useEffect(() => {
     fetchUser()
   }, [fetchUser])
-  
+
   return <>
-  <BrowserRouter>
-  <Routes>
-    <Route element={<PublicRoute />}>
-      <Route path="/login" element={<Login />} />
-    </Route>
-    <Route element={<ProtectedRoute />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/select-role" element={<SelectRole />} />
-    </Route>
-    
-    
-  </Routes>
-  <Toaster />
-  </BrowserRouter>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/select-role" element={<SelectRole />} />
+        </Route>
+
+
+      </Routes>
+      <Toaster />
+    </BrowserRouter>
   </>
-    
-  
+
+
 }
 
 export default App
---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,79 @@
+import { useLocation } from "react-router-dom"
+import { useAppStore } from "../store/AppStore"
+import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { CgShoppingBag, CgShoppingCart } from "react-icons/cg"
+import { BiMapPin, BiSearch } from "react-icons/bi"
+const Navbar = () => {
+
+    const isAuth = useAppStore((state) => state.isAuth)
+    const currLocation = useLocation();
+
+    const isHomePage = currLocation.pathname === "/";
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [search, setSearch] = useState(searchParams.get("search") || "");
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (search) {
+                setSearchParams({ search })
+            }
+            else {
+                setSearchParams({})
+            }
+        }, 400)
+
+        return () => clearTimeout(timer);
+    }, [search]);
+
+    return (
+        <div className="w=full bg-white shadow-sm">
+            <div className="mx-auto flex mx-w-7xl items-center justify-between px-4 py-3">
+                <Link to={'/'} className="text-2xl font-bold text-[#E23744] cursor-pointer">
+                    Foody
+                </Link>
+                <div className="flex items-center gap-4">
+                    <Link to={'/cart'} className="relative">
+                        <CgShoppingCart className="h-6 w-6 text-[#E23744]" />
+                        <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-[#E23744] text-xs font-semibold text-white">
+                            0
+                        </span>
+                    </Link>
+                    {isAuth ? (
+                        <Link to="/account" className="font-medium text-[#E23744]">
+                            Account
+                        </Link>
+                    ) : (
+                        <Link to="/Login" className="font-medium text-[#E23744]">
+                            Login
+                        </Link>
+                    )}
+
+                </div>
+            </div>
+
+            {
+                isHomePage && (
+                    <div className="border-t px-4 py-3">
+                        <div className="mx-auto flex max-w-7xl items-center rounded-lg border shadow-sm">
+                            <div className="flex items-center gap-2 px-3 border-r text-gray-700">
+                                <BiMapPin className="h-4 w-4 text-[#E23744]" />
+                                <span className="text-sm truncate max-w-35">city</span>
+                            </div>
+                            <div className="flex flex-1 items-center gap-2 px-3">
+                                <BiSearch className="h-4 w-4 text-gray-400" />
+                                <input type="text" placeholder="Search for restaurant" value={search}
+                                    onChange={e => setSearch(e.target.value)} className="w-full py-2 text-sm outline-none" />
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+
+        </div>
+    )
+}
+
+export default Navbar
